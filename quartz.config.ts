543903d0e39638476ba17ee70a1b8e20859e5b45
julianhilgemann <hilgemann.julian@gmail.com>
2026-04-10 23:37:44 +0200

    test

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Julian Hilgemann",
+    pageTitle: "Julian Hilgemann - Databites",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
